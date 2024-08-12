f320724185145a99ea51c5563ca819d300bf0dc7
kiyomizuF <shimizu.itsuki@gmail.com>
2024-08-12 22:02:29 +0900

    カルーセル、ヘッダーについての記述を追加

---
+++ js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function () {
+  $(".carousel").not(".slick-initialized").slick({
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDotsHover: false,
+  });
+});
